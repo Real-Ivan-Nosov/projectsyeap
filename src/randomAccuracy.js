6de4810a563ce472d7d65165ab65d9ef922ee9aa
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2021-09-17 19:11:40 +0300

    add new random-checker function

--- src/randomAccuracy.js
+++ src/randomAccuracy.js
@@ -1,5 +1,23 @@
 
-let whatsCount = (iterations, size) => {
+// variant with Math.round, were the chance of getting utmost numbers is half as much as the rest
+
+let whatsChanceByRound = (iterations, size) => {
+    let TableOfCoincidences = new Array(size + 1);
+        for (let j=0; j<TableOfCoincidences.length; j++) {
+            TableOfCoincidences[j] = 0;
+    }
+        for (let i=0; i<iterations; i++) {
+            let Random = Math.round(Math.random() * size);
+            TableOfCoincidences[Random]++;
+    }
+    console.log(TableOfCoincidences);
+}
+
+whatsChanceByRound(1000, 10);
+
+// variant with Math.floor, were the chances of getting all numbers are equal
+
+let whatsChanceByFloor = (iterations, size) => {
     let TableOfCoincidences = new Array(size);
         for (let j=0; j<TableOfCoincidences.length; j++) {
             TableOfCoincidences[j] = 0;
@@ -11,4 +29,4 @@ let whatsCount = (iterations, size) => {
     console.log(TableOfCoincidences);
 }
 
-whatsCount(100000, 10000);
+whatsChanceByFloor(1000, 10);
